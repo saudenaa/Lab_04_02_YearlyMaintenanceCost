f65ceb2ffe03dd4e2fcbc84811d0b07f99dcfb7b
saudenaa <saudenaa@mail.uc.edu>
2024-09-21 09:56:02 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //declearing four season maintenance cost
+
+        double springCost = 60;
+        double summerCost = 50;
+        double fallCost = 80;
+        double winterCost = 90;
+
+        double totalYearlyCost = springCost + summerCost + fallCost + winterCost ;
+
+        System.out.println("spring Cost " + " = " + springCost );
+        System.out.println("summer Cost " + " = " + summerCost);
+        System.out.println("fall cost " + " = " + fallCost);
+        System.out.println("winter cost " + "= " + winterCost);
+        System.out.println("The total yearly maintenance cost of four season is " + totalYearlyCost);
+    }
+}
